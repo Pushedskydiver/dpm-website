e270d0e3f207ca3425bef9d2921169b8d24f86b2
Alex Clapperton <Pushedskydiver@users.noreply.github.com>
2016-02-20 00:34:47 +0000

    Created content page

--- js/script.js
+++ js/script.js
@@ -47,38 +47,81 @@
     // ======================================
     // Accordion jumps
     // ======================================
-    $.fn.lvAccordion = function(){
+    function init_accordion() {
+      $.fn.lvAccordion = function(){
 
-  		var $this = $(this),
-  		    isToggle = $this.hasClass( 'm-toggle' ) ? true : false,
-  		    items = $this.find( '> div' );
+    		var $this = $(this),
+    		    isToggle = $this.hasClass( 'm-toggle' ) ? true : false,
+    		    items = $this.find( '> div' );
 
-  		items.filter( '.m-active' ).find( '.panel-content' ).slideDown( 300 );
+    		items.filter( '.m-active' ).find( '.panel-content' ).slideDown( 300 );
 
-  		$this.find( '.panel-title' ).click(function(){
-  			if ( ! $(this).parent().hasClass( 'm-active' ) ) {
-  				if ( ! isToggle ) {
-  					items.filter( '.m-active' ).find( '.panel-content' ).slideUp(300);
-  					items.filter( '.m-active' ).removeClass( 'm-active' );
-  				}
-  				$(this).parent().find( '.panel-content' ).slideDown(300);
-  				$(this).parent().addClass( 'm-active' );
-  			}
-  			else {
-  				$(this).parent().find( '.panel-content' ).slideUp(300);
-  				$(this).parent().removeClass( 'm-active' );
-  			}
-  		});
+    		$this.find( '.panel-title' ).click(function(){
+    			if ( ! $(this).parent().hasClass( 'm-active' ) ) {
+    				if ( ! isToggle ) {
+    					items.filter( '.m-active' ).find( '.panel-content' ).slideUp(300);
+    					items.filter( '.m-active' ).removeClass( 'm-active' );
+    				}
+    				$(this).parent().find( '.panel-content' ).slideDown(300);
+    				$(this).parent().addClass( 'm-active' );
+    			}
+    			else {
+    				$(this).parent().find( '.panel-content' ).slideUp(300);
+    				$(this).parent().removeClass( 'm-active' );
+    			}
+    		});
 
-  	};
-  	if ( $.fn.lvAccordion ) {
-  		$( '.jump-panel' ).each(function(){
-  			$(this).lvAccordion();
-  		});
-  	}
+    	};
+    	if ( $.fn.lvAccordion ) {
+    		$( '.jump-panel' ).each(function(){
+    			$(this).lvAccordion();
+    		});
+    	}
+    }
+
+
+    // ======================================
+    // Google map - Manchester Art Gallery
+    // ======================================
+    function init_map_mac() {
+      // When the window has finished loading create our google map below
+      google.maps.event.addDomListener(window, 'load', init);
+
+      function init() {
+          // Basic options for a simple Google Map
+          // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+          var mapOptions = {
+              // How zoomed in you want the map to start at (always required)
+              zoom: 15,
+
+              // The latitude and longitude to center the map (always required)
+              center: new google.maps.LatLng(53.4786541, -2.2414114), // Manchester Art Gallery
+
+              // How you would like to style the map.
+              // This is where you would paste any style found on Snazzy Maps.
+              styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"administrative.province","elementType":"labels.icon","stylers":[{"hue":"#ff0000"},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+          };
+
+          // Get the HTML DOM element that will contain your map
+          // We are using a div with id="map" seen below in the <body>
+          var mapElement = document.getElementById('map-mac');
+
+          // Create the Google Map using our element and options defined above
+          var map = new google.maps.Map(mapElement, mapOptions);
+
+          // Let's also add a marker while we're at it
+          var marker = new google.maps.Marker({
+              position: new google.maps.LatLng(53.4786541, -2.2414114),
+              map: map,
+              icon: "http://s15.postimg.org/9mx28u5bb/pointer.png"
+          });
+      }
+    }
 
     init_smooth_scroll();
     init_smooth_scroll_top();
+    init_accordion();
+    init_map_mac();
 
     var WSD = function() {
         this.VERSION = "1.1.0";
